f1b507fde3b540c867a3c237f6416f6747e32efe
Stefan Hagen <stefan@hagen.link>
2022-07-10 17:47:57 +0200

    Adapted to union concept

--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
-# -*- coding: utf-8 -*-
-import pathlib
-from setuptools import find_packages, setup
+import setuptools
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
-
-# This call to setup() does all the work
-setup(
-    name="sixties",
-    version="0.0.1",
-    description="Parsing of fixed width text databases derived in the Seventies from the Sixties' spirit of the previous century.",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sdrees/sixties",
-    author="Stefan Hagen",
-    author_email="stefan@hagen.link",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-    ],
-    packages=find_packages(exclude=("tests",)),
-    include_package_data=True,
-    install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "sixties=sixties.cli:main",
-        ]
-    },
-)
+setuptools.setup()
