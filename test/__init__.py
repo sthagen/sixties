26e6a65b3176788ffa7489a457223710444d40cf
Stefan Hagen <stefan@hagen.link>
2022-07-10 17:48:16 +0200

    Adapted to union concept

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=missing-docstring
 
-__version__ = "0.0.1"
+__version__ = '0.0.1'
 
 """Sixties test package initialisation."""
--- test/test_cli.py
+++ test/test_cli.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=missing-docstring,unused-import,reimported
-import sixties.cli as cli
+from sixties import cli
 
 
 def test_cli_main_ok():
-    argv = ["ignored", "printed"]
+    argv = ['ignored', 'printed']
     assert cli.main(argv) == argv
