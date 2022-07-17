48630604579c5846e9bd499f6b85ba117a9240f7
Stefan Hagen <stefan@hagen.link>
2022-07-17 13:13:01 +0200

    Bumped impl version (WIP)

--- sixties/__init__.py
+++ sixties/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # [[[fill git_describe()]]]
-__version__ = '2022.7.10+parent.abadcafe'
+__version__ = '2022.7.17'
 # [[[end]]]
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
