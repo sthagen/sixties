1b5a3e08e2a1e2d0df478044331d70f117401dcf
Stefan Hagen <stefan@hagen.link>
2022-07-17 13:08:15 +0200

    Types pass

--- sixties/__main__.py
+++ sixties/__main__.py
@@ -4,4 +4,4 @@ import sys
 from sixties.cli import main
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))  # pragma: no cover  # type: ignore
+    sys.exit(main(sys.argv))  # pragma: no cover  # types: ignore
--- sixties/cli.py
+++ sixties/cli.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """Command line arg for sixties."""
 import sys
+from typing import List, Union
 
 
-def main(argv=None):  # type: ignore
+def main(argv: Union[List[str], None] = None) -> int:
     """Drive the understanding ..."""
     argv = sys.argv if argv is None else argv
-
-    return argv
+    print(argv)
+    return 0
