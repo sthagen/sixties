0b830fbc4b433a983175d376eddb712f8f89add0
Stefan Hagen <stefan@hagen.link>
2022-07-17 13:10:55 +0200

    Made the test pass again

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,6 +2,9 @@
 from sixties import cli
 
 
-def test_cli_main_ok():
+def test_cli_main_ok(capsys):
     argv = ['ignored', 'printed']
-    assert cli.main(argv) == argv
+    assert cli.main(argv) == 0
+    out, err = capsys.readouterr()
+    assert out.strip() == str(argv)
+    assert not err
